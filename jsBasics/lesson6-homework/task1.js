6f7abdb497b39c3e7d9b3fdef371b72c742caf0b
Oleksandr Shcherbatiuk <sashakaroch@gmail.com>
2025-12-23 15:16:45 +0100

    Lesson 6 homework: functions

---
+++ jsBasics/lesson6-homework/task1.js
@@ -0,0 +1,20 @@
+function rectangleArea1(width, height) {
+  return width * height;
+}
+
+console.log(rectangleArea1(5, 10));
+
+
+
+const rectangleArea2 = function (width, height) {
+  return width * height;
+};
+
+console.log(rectangleArea2(5, 10));
+
+
+
+
+const rectangleArea3 = (width, height) => width * height;
+
+console.log(rectangleArea3(5, 10));
