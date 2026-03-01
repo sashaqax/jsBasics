9875df6bbee7f848b9fb99c3b2af99893175c6eb
Oleksandr Shcherbatiuk <sashakaroch@gmail.com>
2026-03-02 00:13:51 +0100

    refactor: улучшение стиля кода и форматирования в async.js и bonus-task6.js

--- jsBasics/lesson11/async.js
+++ jsBasics/lesson11/async.js
@@ -2,37 +2,31 @@
 //let result1 = 5 + 5;
 //console.log(`Result 1:${result1}`);
 
-
-
 //console.log('Operation 2: ');
 //let result2 = 51 + 5;
 //console.log(`Result 2:${result2}`);
-
 
 //console.log('Operation 3: ');
 //let result3 = 51 + 5;
 //console.log(`Result 3:${result3}`);
 
-
-
 newFunction();
 
 function newFunction() {
-    const promiseA = Promise.resolve('Hello');
-    const promiseB = Promise.resolve('Promise');
-    const promiseC = Promise.resolve('All');
-    const promisesCollection = Promise.all([promiseA, promiseB, promiseC]);
+  const promiseA = Promise.resolve('Hello');
+  const promiseB = Promise.resolve('Promise');
+  const promiseC = Promise.resolve('All');
+  const promisesCollection = Promise.all([promiseA, promiseB, promiseC]);
 
-    promisesCollection.then(result => console.log(result));
+  promisesCollection.then((result) => console.log(result));
 }
 
-
 const differedPromise = new Promise((resolve) => {
-    setTimeout(() => {
-        resolve('Diferred Hello');
-    }, 2000);
+  setTimeout(() => {
+    resolve('Diferred Hello');
+  }, 2000);
 });
-differedPromise.then(result => console.log(result));
+differedPromise.then((result) => console.log(result));
 
 console.log('Operation 3: ');
 const result4 = 511 + 5;
@@ -42,4 +36,4 @@ console.log('Operation 4: ');
 const result5 = 5111 + 5;
 console.log(`Result 5:${result5}`);
 
-console.log 
+console.log;
